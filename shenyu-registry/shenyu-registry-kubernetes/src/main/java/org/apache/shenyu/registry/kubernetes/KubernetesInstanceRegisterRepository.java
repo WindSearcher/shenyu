05f3de127e7b9a248037bbe4d4c21f1b23f445ec
WindSearcher <1793521005@qq.com>
2024-06-26 16:34:28 +0800

    feature:kubernetes registry center

--- shenyu-registry/shenyu-registry-kubernetes/src/main/java/org/apache/shenyu/registry/kubernetes/KubernetesInstanceRegisterRepository.java
+++ shenyu-registry/shenyu-registry-kubernetes/src/main/java/org/apache/shenyu/registry/kubernetes/KubernetesInstanceRegisterRepository.java
@@ -1,27 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shenyu.registry.kubernetes;
 
 import org.apache.shenyu.registry.api.ShenyuInstanceRegisterRepository;
 import org.apache.shenyu.registry.api.config.RegisterConfig;
 import org.apache.shenyu.registry.api.entity.InstanceEntity;
 import org.apache.shenyu.spi.Join;
-import org.checkerframework.checker.units.qual.K;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClient;
-import org.springframework.cloud.kubernetes.discovery.KubernetesDiscoveryClientProperties;
-import org.springframework.cloud.kubernetes.discovery.KubernetesServiceInstance;
-
-import java.net.URI;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
 /**
- * @description The type kubernetes instance register repository
- * @author windsearcher.lq
- * @date 2024/6/24 11:01
+ * The type kubernetes instance register repository.
  */
 @Join
 public class KubernetesInstanceRegisterRepository implements ShenyuInstanceRegisterRepository {
